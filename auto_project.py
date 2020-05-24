c570bfc20e2d767e0212775dd7bc916d1c0cc62b
Powerbuilder <philipp.120201@gmail.com>
2020-05-24 15:04:11 +0200

    create python file

---
+++ auto_project.py
@@ -0,0 +1,14 @@
+from github import Github
+import sys
+import dotenv
+
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    print("Args", args)
+
+
+    # g = Github("username", "password")
+    # user = g.get_user()
+    # user.create_repo(repo_name)
