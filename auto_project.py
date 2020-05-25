7ee7ec4f08ef0e7a19e377a6d387584e618d139d
Powerbuilder <philipp.120201@gmail.com>
2020-05-25 21:01:23 +0200

    create func. / set_size_and_pos

--- auto_project.py
+++ auto_project.py
@@ -47,39 +47,29 @@ def open_git_kraken():
     # write command in new terminal
     pyautogui.typewrite(all_letters)
 
+def set_size_and_pos(width, height, x, y):
+    # get window id
+    command = "xdotool search --onlyvisible --name gitkraken"
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = proc.communicate()
+    # get list of all window_ids
+    window_ids = str(stdout, "utf8").split("\n")[:-1]
+    # set focus window
+    if len(window_ids) == 0:
+        print("[ERROR] No window found!")
+    command = f"xdotool windowsize {width} {height} {window_ids[0]}"
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = proc.communicate()
+    command = f"xdotool windowmove {window_ids[0]} {x}, {y}"
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = proc.communicate()
+
+
 def clone_repo_git_kraken(clone_url):
     # hotkey for git clone
     pyautogui.hotkey('ctrl', 'n')
 
-    # # get window id
-    # command = "xdotool search --onlyvisible --name gitkraken"
-    # proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # (stdout, stderr) = proc.communicate()
-    # # get list of all window_ids
-    # window_ids = str(stdout, "utf8").split("\n")[:-1]
-    # for window_id in window_ids:
-    #     # set focus window
-    #     command = f"xdotool windowfocus {window_id}"
-    #     proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #     (stdout, stderr) = proc.communicate()
-
-    # tab to "Where to clone" input field
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-
+    #
 
     # list of letters in command
     all_letters = []
@@ -142,6 +132,27 @@ def create():
     clone_repo_git_kraken(url)
 
 
+    # # terminal with project path
+    # pyautogui.hotkey('ctrl', 'shift', 'n')
+    #
+    # command = f"cd ~/Dokumente/Projekte/Project_automation/{repo_name}/"
+    # # list of letters in command
+    # all_letters = []
+    # for letter in command:
+    #     # avoid getting 7 instead of /
+    #     if letter == '/':
+    #         all_letters.append('divide')
+    #     else:
+    #         all_letters.append(letter)
+    # # add letter to submit command
+    # all_letters.append("enter")
+    # # write command in new terminal
+    # pyautogui.typewrite(all_letters)
+
+
+
 
 if __name__ == "__main__":
-    create()
+    # create()
+
+    set_size_and_pos(800, 800, 0, 0)
